a71c4d8199995fa9dee330c4c4c8719177525cef
prodmodfour <prodmodfour@users.noreply.github.com>
2026-01-28 14:14:03 +0000

    Updates

---
+++ assets/md-BoXQo2A9.js
@@ -0,0 +1,11 @@
+import{_ as o}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-DDzj1wsZ.js";import{o as p,b as c,w as e,g as s,d,m as u,ad as a,v as m,x as f,T as l}from"./modules/vue-XRI2LF4Y.js";import{I as g}from"./slidev/default-f5HTpZEE.js";import{u as k,f as _}from"./slidev/context-DpzpJE6C.js";import"./modules/unplugin-icons-CpLqI1zF.js";import"./index-DwpEi8o7.js";import"./modules/shiki-B5VYq2F5.js";const y={__name:"slides.md__slidev_4",setup(v){const{$clicksContext:t,$frontmatter:i}=k();return t.setup(),(h,n)=>{const r=o;return p(),c(g,m(f(l(_)(l(i),3))),{default:e(()=>[n[1]||(n[1]=s("h1",null,"Architecture Overview",-1)),d(r,u({},{title:"",ranges:[]}),{default:e(()=>[...n[0]||(n[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"ada-ui (Svelte)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    |")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    v")]),a(`
+`),s("span",{class:"line"},[s("span",null,"ada-api (FastAPI proxy)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    |")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    v")]),a(`
+`),s("span",{class:"line"},[s("span",null,"ada-carbon-monitoring-api (FastAPI)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    |")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    +---> Prometheus (CPU metrics)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    +---> MongoDB via ada-db-interface (workspace data)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    +---> UK Carbon Intensity API (grid carbon)")])])],-1)])]),_:1},16)]),_:1},16)}}};export{y as default};
---
+++ assets/md-vuLyOqMl.js
@@ -0,0 +1,13 @@
+import{_ as o}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-DDzj1wsZ.js";import{o as p,b as c,w as e,g as s,d as u,m as d,ad as n,v as m,x as f,T as l}from"./modules/vue-XRI2LF4Y.js";import{I as _}from"./slidev/default-f5HTpZEE.js";import{u as k,f as h}from"./slidev/context-DpzpJE6C.js";import"./modules/unplugin-icons-CpLqI1zF.js";import"./index-DwpEi8o7.js";import"./modules/shiki-B5VYq2F5.js";const E={__name:"slides.md__slidev_7",setup(g){const{$clicksContext:i,$frontmatter:t}=k();return i.setup(),(x,a)=>{const r=o;return p(),c(_,m(f(l(h)(l(t),6))),{default:e(()=>[a[1]||(a[1]=s("h1",null,"Calculation Pipeline",-1)),u(r,d({},{title:"",ranges:[]}),{default:e(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"CPU Seconds (Prometheus)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Electricity (kWh)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    Formula: (12W x busy_seconds + 1W x idle_seconds) / 3,600,000")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Carbon (gCO2eq)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    Formula: kWh x carbon_intensity (from UK Grid API)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    v")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Equivalencies")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    Example: miles driven, smartphone charges, tree-days")])])],-1)])]),_:1},16)]),_:1},16)}}};export{E as default};
